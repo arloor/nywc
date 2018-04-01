bd68868d6a828ee766f453a74b33930704320823
arloor <admin@arloor.com>
2018-04-01 23:15:15 +0800

    获取openid

---
+++ jianbujing/src/main/java/com/arloor/jianbujing/service/WeixinService.java
@@ -0,0 +1,30 @@
+package com.arloor.jianbujing.service;
+
+import com.google.gson.JsonObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/weixin")
+public class WeixinService {
+
+    @Value("${weixin.appid}")
+    private String appid;
+    @Value("${weixin.appsecret}")
+    private String appsecret;
+
+    //获取用户的openid
+    @RequestMapping("/openid")
+    public String openid(@RequestParam String js_code) {
+        Mono<String> resp = WebClient.create()
+                .get()
+                .uri("https://api.weixin.qq.com/sns/jscode2session?appid={appid}&secret={secret}&grant_type=authorization_code&js_code={js_code}",appid,appsecret,js_code) //使用占位符
+                .retrieve()
+                .bodyToMono(String.class);
+        return resp.block();
+    }
+}
--- jianbujing_xiaochengxu/pages/index/index.js
+++ jianbujing_xiaochengxu/pages/index/index.js
@@ -6,8 +6,7 @@ Page({
   data: {
     motto: '南大IP',
     userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    openId:null
   },
   //事件处理函数
   bindViewTap: function () {
@@ -21,15 +20,58 @@ Page({
     })
   },
   onLoad: function () {
-    var that=this;
-    wx.getUserInfo({
+    //获得用户数据，并设置到app.globalData
+    var that = this;
+    wx.login({
       success: function (res) {
-        app.globalData.userInfo = res.userInfo;
-        that.setData({userInfo: res.userInfo})
-      },
-      fail: function (failData) {
-        console.info("用户拒绝授权");
+        if (res.code) {
+          // //获取openId
+          // wx.request({
+          //   url: 'https://api.weixin.qq.com/sns/jscode2session',
+          //   data: {
+          //     //小程序唯一标识
+          //     appid: '',
+          //     //小程序的 app secret
+          //     secret: '',
+          //     grant_type: 'authorization_code',
+          //     js_code: res.code
+          //   },
+          //   method: 'GET',
+          //   header: { 'content-type': 'application/json' },
+          wx.request({
+            url:'https://jianbujing.moontell.cn/api/weixin/openid?js_code='+res.code,
+            method:"get",
+            success: function (openIdRes) {
+              // console.info("登录成功返回的openId：" + openIdRes.data.openid);
+
+              // 判断openId是否获取成功
+              if (openIdRes.data.openid != null & openIdRes.data.openid != undefined) {
+                app.globalData.openId = openIdRes.data.openid;
+                that.setData({ openId: openIdRes.data.openid});
+                console.info("openId： " + that.data.openId);
+                wx.getUserInfo({
+                  success: function (res) {
+                    app.globalData.userInfo = res.userInfo;
+                    that.setData({userInfo:res.userInfo});
+                    console.info("nickName： " + that.data.userInfo.nickName);
+                    console.info("avatarUrl: " + that.data.userInfo.avatarUrl);
+                  },
+                  fail: function (failData) {
+                    console.info("用户拒绝授权");
+                  }
+                })
+              } else {
+                console.info("获取用户openId失败");
+              }
+            },
+            fail: function (error) {
+              console.info("获取用户openId失败");
+              console.info(error);
+            }
+          })
+        }
       }
-    })
+    });
+    //获得用户数据，并设置到app.globalData-----结束
   }
 })
