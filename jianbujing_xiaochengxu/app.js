f12a7eee7c17574338da6f04ceca6e43261f3085
arloor <admin@arloor.com>
2018-04-01 21:25:44 +0800

    获取openid

--- jianbujing_xiaochengxu/app.js
+++ jianbujing_xiaochengxu/app.js
@@ -5,27 +5,49 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-  },
-  getUserInfo:function(cb){
+
+    //
     var that = this;
-    if(this.globalData.userInfo){
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    }else{
-      //调用登录接口
-      wx.login({
-        success: function () {
-          wx.getUserInfo({
-            success: function (res) {
-              that.globalData.userInfo = res.userInfo;
-              typeof cb == "function" && cb(that.globalData.userInfo)
+    wx.login({
+      success: function (res) {
+        if (res.code) {
+          //获取openId
+          wx.request({
+            url: 'https://api.weixin.qq.com/sns/jscode2session',
+            data: {
+              //小程序唯一标识
+              appid: 'wx4dc6362275b751ba',
+              //小程序的 app secret
+              secret: 'b7906e3dfa3aa49eb56764e9697056d6',
+              grant_type: 'authorization_code',
+              js_code: res.code
+            },
+            method: 'GET',
+            header: { 'content-type': 'application/json' },
+            success: function (openIdRes) {
+              console.info("登录成功返回的openId：" + openIdRes.data.openid);
+              
+              // 判断openId是否获取成功
+              if (openIdRes.data.openid != null & openIdRes.data.openid != undefined) {
+                that.globalData.openId = openIdRes.data.openid;
+                console.info("app.globalData.openId： "+that.globalData.openId);
+              } else {
+                console.info("获取用户openId失败");
+              }
+            },
+            fail: function (error) {
+              console.info("获取用户openId失败");
+              console.info(error);
             }
           })
         }
-      });
-    }
+      }
+    });
+    //
   },
   globalData:{
-    userInfo:null
+    userInfo:null,
+    openId:null
   }
 })
 
