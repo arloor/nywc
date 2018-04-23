54271ea6e728372a836a9ee48338dd516805e8e3
arloor <admin@arloor.com>
2018-04-23 12:30:39 +0800

    小改

--- nywc/src/main/java/com/arloor/nywc/config/MyBatisConfig.java
+++ nywc/src/main/java/com/arloor/nywc/config/MyBatisConfig.java
@@ -7,12 +7,17 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
 import javax.sql.DataSource;
 
 @Configuration
+@EnableTransactionManagement//支持事务
 @ComponentScan
-public class MyBatisConfig {
+public class MyBatisConfig implements TransactionManagementConfigurer {
 
     @Autowired
     private DataSource dataSource;
@@ -23,6 +28,12 @@ public class MyBatisConfig {
         sessionFactory.setDataSource(dataSource);
         sessionFactory.setMapperLocations(applicationContext.getResources("classpath*:mapper/*.xml"));
         return sessionFactory;
+    }
+
+    @Bean//事务bean
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return new DataSourceTransactionManager(dataSource);
     }
 
 }
