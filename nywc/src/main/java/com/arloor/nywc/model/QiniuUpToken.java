fdcd6666e6edab0adce9fcd9711f2d99c24b5c9c
arloor <admin@arloor.com>
2018-06-09 11:41:01 +0800

    增加获取七牛云token的代码

---
+++ nywc/src/main/java/com/arloor/nywc/model/QiniuUpToken.java
@@ -0,0 +1,13 @@
+package com.arloor.nywc.model;
+
+public class QiniuUpToken {
+    private String uptoken;
+
+    public String getUptoken() {
+        return uptoken;
+    }
+
+    public void setUptoken(String uptoken) {
+        this.uptoken = uptoken;
+    }
+}
